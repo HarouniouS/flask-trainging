7e5f4567a3c06498b0cd890dcc267c68ac480943
HarouniouS <saif.alharoun.2017@gmail.com>
2021-07-07 21:52:03 +0300

    fourth commit

--- flaskblog.py
+++ flaskblog.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2d4904a0818e55ebf6d4c38e8af9b56c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model) :
