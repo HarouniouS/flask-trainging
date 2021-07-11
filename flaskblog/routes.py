3d95a59957ec7c7294d63cca326168e3e42f5c3e
HarouniouS <saif.alharoun.2017@gmail.com>
2021-07-11 22:20:55 +0300

    more features of posting (dynamic)

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,28 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, request, redirect, request
 from flaskblog.models import User, Post
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Fuad Daoud',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 22, 2020'
-    },
-    {
-        'author': 'Saif AlHaroun',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 21, 2020'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts, title='Home')
 
 @app.route("/about")
@@ -95,3 +82,20 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form = form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
