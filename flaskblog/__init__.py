fb660f30b76c1df1524b87302786f19421b30435
HarouniouS <saif.alharoun.2017@gmail.com>
2021-07-13 17:56:09 +0300

    finished almost

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,10 @@
+import os
+import smtplib
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2d4904a0818e55ebf6d4c38e8af9b56c'
@@ -11,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'flaskblog2021@outlook.com'
+app.config['MAIL_PASSWORD'] = 'Flaskblog.2021'
+mail = Mail(app)
 
 from flaskblog import routes
